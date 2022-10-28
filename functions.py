c1a5c3de61f3bd12e1b32ed37f7f9a8ee3f8aed7
Matteo Marino <95318540+Mathieus99@users.noreply.github.com>
2022-10-29 00:17:02 +0200

    Cambio nome

---
+++ functions.py
@@ -0,0 +1,9 @@
+def cmd_list():
+    print ("\nLista comandi\n")
+    print ("os\tVisualizza informazioni sul sistema operativo\n")
+    print ("ls\tVisualizza il contenuto della directory corrente\n")
+    print ("cd\tConsente di spostarsi in un altra drectory specificata dal path\n")
+    print ("pwd\tConsente di visualizzare il path della directory corrente\n")
+    print ("system\tVisualizza informazioni sulla macchina(CPU, memoria ecc...)\n")
+
+    
